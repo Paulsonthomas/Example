6b9edaef92ae31bdcb026eb8ff5e9add090d1765
Paulsonthomas <paulsonthomas14082001@gmail.com>
2023-12-14 13:48:35 +0530

    Saved

--- src/com/jsp/example/User.java
+++ src/com/jsp/example/User.java
@@ -2,4 +2,37 @@ package com.jsp.example;
 
 public class User {
 
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
 }
